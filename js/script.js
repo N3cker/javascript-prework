3c36d9c016b987545ff03491f995508f3f854d37
Mateusz Piasecki <piaszuu@gmail.com>
2019-10-03 22:24:09 +0200

    Add code in script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-function printMessage(msg){
+let computerMove = `kamień`;
+let playerMove = `papier`;
+
+function printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!'){
 	var div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
